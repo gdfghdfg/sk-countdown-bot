86de46263f0fea15aef816638223b6256a393f56
gdfghdfg <work.sknetlk@gmail.com>
2025-05-05 11:22:04 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
 
 # Exam date
-exam_date = datetime.datetime(2025, 10, 21)
+exam_date = datetime.datetime(2025, 10, 10)
 today = datetime.datetime.today()
 days_left = (exam_date - today).days
 
-message = f"📢 අද දිනය: {today.strftime('%Y-%m-%d')}\n2025 A/L ට දිනයන් {days_left}යි! 📚💪"
+message = f"2025 A/L ට දිනයන් {days_left}යි! 📚💪📢\n\n\n\n\n අද දිනය: {today.strftime('%Y-%m-%d')}"
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 payload = {'chat_id': CHAT_ID, 'text': message}
